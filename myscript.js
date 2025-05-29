a90261d6ac6ee9d0f26d77b4ac5b0ba12506a7ed
shwetakiit <shwetakumarikiit0729@gmail.com>
2025-05-29 23:45:49 +0530

    DailogPopup

---
+++ myscript.js
@@ -0,0 +1,51 @@
+
+function  myFunction() {
+ 
+    var name ='shweta';
+    var name;   //Redclaration of variable will pick still pick shweta
+
+
+    document.getElementById("demo").innerHTML=name;
+  //  document.writeln("shweta")   It will override whole document
+
+}
+
+
+function generateTable(){
+
+
+    const tbl = document.createElement("table");
+  const tblBody = document.createElement("tbody");
+
+  // creating all cells
+  for (let i = 0; i < 4; i++) {
+    // creates a table row
+    const row = document.createElement("tr");
+
+    for (let j = 0; j < 4; j++) {
+      // Create a <td> element and a text node, make the text
+      // node the contents of the <td>, and put the <td> at
+      // the end of the table row
+      const cell = document.createElement("td");
+      const cellText = document.createTextNode(`cell in row ${i}, column ${j}`);
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    }
+
+    // add the row to the end of the table body
+    tblBody.appendChild(row);
+  }
+
+  // put the <tbody> in the <table>
+  tbl.appendChild(tblBody);
+  // appends <table> into <body>
+  document.body.appendChild(tbl);
+  // sets the border attribute of tbl to '2'
+  tbl.setAttribute("border", "2");
+
+
+
+
+}
+
+
